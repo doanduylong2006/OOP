3fbfb33cb282432d26d1b36c0173110a7cfb6078
doanduylong2006 <doanduylong26102006@gmail.com>
2025-09-18 23:04:57 +0700

    Create Employee.cs

---
+++ oop-main/HW3/Employee.cs
@@ -0,0 +1,14 @@
+public class Employee
+{
+    private string name;
+
+    public Employee(string name)
+    {
+        this.name = name;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
